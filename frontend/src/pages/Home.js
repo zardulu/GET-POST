59d16c097dbc5f0e1be2252aaccf141e6c0c5b17
zardulu <iktftbh@protonmail.com>
2023-07-26 04:16:59 +0530

    Add logo

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, TextField, Grid, Divider, Link } from '@mui/material';
+import { Grid, Divider, Link } from '@mui/material';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import axios from 'axios';
 import NewPostForm from '../components/NewPostForm';
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns'; 
+import logo from '../assets/logo3.svg';
+
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
+  // Fetches posts  
   useEffect(() => {
     axios.get('http://localhost:5000/api/posts/')
       .then(response => {
@@ -18,29 +21,40 @@ const Home = () => {
         console.log(error);
       });
   }, []);
-
+  
+  // Converts to more readable date format
   const formatTimeDistance = (date) => {
     return formatDistanceToNow(new Date(date), { addSuffix: true });
   };
 
   return (
-    <div>
-      <NewPostForm />
+    <div>  
+      
+      <img src={logo} alt='logo' style={{ width: '120px', height: '60px', margin: '15px' }}/> {/* Logo */}
+      <NewPostForm /> { /* New post input form */}
+
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={6} >
           
+          {/* Maps posts to title links */}
           {posts.map(post => (
             <div key={post._id} style={{ marginBottom: '50px' }}>
+
               <div style={{ display: 'flex', alignItems: 'left' }}>
-              <FormatQuoteIcon color="primary" sx={{ marginBottom: '50px' }} />
-              <Link component={RouterLink} to={`/post/${post._id}`} sx={{ textDecoration:'none', color: 'white', '&:hover': {
-            color: '#39FF14'} }} >
-              <h3 style={{ marginLeft: '10px' }}>{post.title}</h3>
-              </Link>
-            </div>
+              
+              <FormatQuoteIcon color="primary" sx={{ marginBottom: '50px' }} /> {/* bullet point icon */}
+
+                <Link component={RouterLink} to={`/post/${post._id}`} sx={{ textDecoration:'none', color: 'white', '&:hover': {
+                color: '#39FF14'} }}>
+                  <h3 style={{ marginLeft: '10px' }}>{post.title}</h3>
+                </Link>
+
+              </div>
+
               <p style={{ textAlign:'right', fontStyle: 'italic', color: 'grey' }}>{formatTimeDistance(post.date)}</p>
-              <Divider sx={{ bgcolor: "secondary.light" }} />
+              <Divider sx={{ bgcolor: "secondary.light" }} /> 
             </div>
+
           ))}
         </Grid>
       </Grid>
--- frontend/src/pages/ViewPost.js
+++ frontend/src/pages/ViewPost.js
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import Post from '../components/Post';
 import NewCommentForm from '../components/NewCommentForm';
 import Comment from '../components/Comment';
-
+import logo from '../assets/logo3.svg';
+import { Link } from 'react-router-dom';
 
 
 const ViewPost = () => {
@@ -11,9 +12,13 @@ const ViewPost = () => {
 
   return (
     <div>
-      {/* Render the Post component and pass the id as a prop */}
+      <Link to='/Home'>
+      <img src={logo} alt='logo' style={{ width: '120px', height: '60px', margin: '15px' }}/>
+      </Link>
+
+      {/* Render Post component and pass the id as prop */}
       <Post postId={id} />
-      <NewCommentForm />
+      <NewCommentForm postId={id} />
       <Comment postId={id} />
     </div>
   );
