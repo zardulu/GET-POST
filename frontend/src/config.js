ad91b2b1f75fcb41830c1e6b265422beeafa3d17
zardulu <iktftbh@protonmail.com>
2023-07-29 22:31:29 +0530

    Add config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,12 @@
+
+const config = {
+    development: {
+      apiUrl: process.env.REACT_APP_DEV_API_URL,
+    },
+    production: {
+      apiUrl: process.env.REACT_APP_PROD_API_URL,
+    },
+  };
+  
+  export default config;
+  
